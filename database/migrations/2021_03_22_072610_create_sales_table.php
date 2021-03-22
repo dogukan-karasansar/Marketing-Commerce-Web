9d53391d712a7327687767f6d0f99aefcf169c6d
Doğukan K <dogukan_14072002@hotmail.com>
2021-03-22 11:19:59 +0300

    migration created

---
+++ database/migrations/2021_03_22_072610_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer');
+            $table->unsignedBigInteger('product');
+            $table->double('price');
+            $table->text('addresses');
+            $table->text('phone');
+            $table->timestamps();
+            $table->foreign('customer')->references('id')->on('customers');
+            $table->foreign('product')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
